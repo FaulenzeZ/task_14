ea28b1e9b503a21115a6bb10ed997531216b3b9d
FaulenzeZ <idrisov3688066@gmail.com>
2026-09-21 13:32:33 +0700

    cola

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] mass = Console.ReadLine().Split(' ');
+        int G = int.Parse(mass[0]);
+        int L = int.Parse(mass[1]);
+        int max = (G + L) - 1;
+        int _G = max - G;
+        int _L = max - L;
+        Console.WriteLine($"{_G} {_L}");
+    }
+}
